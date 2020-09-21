2c93454ddc97fb25dd9783928db5a6b67c498ec2
Jonathan Furrer <jonathan.furrer@gmail.com>
2020-09-21 23:45:35 +0200

    init / Azure Table Storage works

---
+++ readme.md
@@ -0,0 +1,6 @@
+Benötigte Pakete:
+
+Azure:
+
+pip install --pre azure-data-tables // https://pypi.org/project/azure-data-tables/12.0.0b1/
+
---
+++ stromabrechnung.py
@@ -0,0 +1,15 @@
+from azure.cosmosdb.table.tableservice import TableService
+from azure.cosmosdb.table.models import Entity
+
+credential = "eFH0EUaE6HU8GaqS9S04vf2MLdQ94vMdbMg6kaMetKG1gXU8g+QUtfAKxbtvyjj6Vrq4X3E0hBVNe5gIzjFZHQ=="
+
+table_service = TableService(account_name="jofustrom456789", account_key=credential)
+
+task = Entity()
+task.PartitionKey = 'tasksSeattle'
+task.RowKey = '002'
+task.description = 'Wash the car'
+task.priority = 100
+table_service.insert_entity('D41Strombezug', task)
+
+
