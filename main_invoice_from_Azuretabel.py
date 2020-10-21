baa80d266c04a19115dcca02a8ebfc8f56e6e070
Jonathan Furrer <j.furrer@unico.ch>
2020-10-21 21:51:23 +0200

    added Azure Function / invoice now with functions. finished

--- main_invoice_from_Azuretabel.py
+++ main_invoice_from_Azuretabel.py
@@ -47,109 +47,102 @@ except:
     logging.warning("could not connect to Azure Table Storage")
     exit
 
-# Get newest Adress Data from Azure
-try:
-    adress = table_service.get_entity(TABLE_ADRESSEN,'272-1','1')
-    adressData272_1 = CustomerData(adress.Wohnung,adress.Vorname,adress.Name,adress.Adresse,adress.Ort,adress.PLZ,adress.email)
-except:
-    logging.warning("failed to load adress data from azure table 272-1")
+def getAdressData(table,partitionKey,rowKey):
+    try:
+        adress = table_service.get_entity(table,partitionKey,rowKey)
+        return CustomerData(adress.Wohnung,adress.Vorname,adress.Name,adress.Adresse,adress.Ort,adress.PLZ,adress.email)
+    except:
+        logging.warning("failed to load adress data from "+ table + " PartitionKey: " + partitionKey + " RowKey: " + rowKey)
+        return None 
 
-try:
-    adress = table_service.get_entity(TABLE_ADRESSEN,'272-2','2')
-    adressData272_2 = CustomerData(adress.Wohnung,adress.Vorname,adress.Name,adress.Adresse,adress.Ort,adress.PLZ,adress.email)
-except:
-    logging.warning("failed to load adress data from azure table 272-2")
+def getBankData(table,partitionKey,rowKey):
+    try:
+        bank = table_service.get_entity(table,partitionKey,rowKey)
+        return BankData(namebank=bank.NameBank, iban=bank.IBAN,kontonr=bank.KontoNr,inhaber1=bank.Inhaber1,inhaber2=bank.Inhaber2)
+    except:
+        logging.warning("failed to load adress data from "+ table + " PartitionKey: " + partitionKey + " RowKey: " + rowKey)
+        return None 
 
-try:
-    adress = table_service.get_entity(TABLE_ADRESSEN,'272-3','3')
-    adressData272_3 = CustomerData(adress.Wohnung,adress.Vorname,adress.Name,adress.Adresse,adress.Ort,adress.PLZ,adress.email)
-except:
-    logging.warning("failed to load adress data from azure table 272-3")
+def getInvoice(t_strombezug,t_preise,adr_kunde, adr_an, bank_data):
+    # Get Invoice information
+    f = "PartitionKey eq '" + adr_kunde.wohnung + "' and cleared eq false"        
+    try:
+        open_invoice = table_service.query_entities(t_strombezug, filter=f , timeout=60)
+    except:
+        logging.warning("failed to load adress data from "+ t_strombezug + " PartitionKey: " + adr_kunde.Wohnung)
 
-try:
-    adress = table_service.get_entity(TABLE_ADRESSEN, '272-4','4')
-    adressData272_4 = CustomerData(adress.Wohnung,adress.Vorname,adress.Name,adress.Adresse,adress.Ort,adress.PLZ,adress.email)
-except:
-    logging.warning("failed to load adress data from azure table 272-4")
+    total = 0
+    line = 1
+    open_invoice = list(open_invoice)
+    inv = generate_invoice(address_kunde=adr_kunde,address_an=adr_an,bank=bank_data)
 
-# Bank --------------------------------------------------
-try:
-    bank = table_service.get_entity(TABLE_BANKVERBINDUNG,'Universalkonto','1')
-    bankData = BankData(namebank=bank.NameBank, iban=bank.IBAN,kontonr=bank.KontoNr,inhaber1=bank.Inhaber1,inhaber2=bank.Inhaber2)
-except:
-    logging.warning("failed to load adress data from azure table Bankverbindung")
+    for oi in open_invoice:
+        # Preise ------------------------------------------------
+        # Grundgebühr
+        f = "PartitionKey eq 'Grundgebühr' and Year eq " + str(oi.year)
+        try:
+            grundgebühr = table_service.query_entities(t_preise, filter=f , timeout=60)
+            grundgebühr = list(grundgebühr) 
+        except:
+            logging.warning("failed to load adress data from azure table " + t_preise)
+        
+        
+        if "day" in oi.tarif:
+            # Hochtarif / Tag
+            f = "PartitionKey eq 'Hochtarif' and Year eq " + str(oi.year)
+            try:
+                hochtarif = table_service.query_entities(t_preise, filter=f , timeout=60)
+                hochtarif = list(hochtarif)
+            except:
+                logging.warning("failed to load adress data from azure table " + t_preise)
+
+            tarif = "Hochtarif / Tag"
+            preis = str(hochtarif[0].Preis) + " " +  str(hochtarif[0].Währung)
+            total += (hochtarif[0].Preis * oi.value)
+            subtotal = str(round(hochtarif[0].Preis * oi.value,2)) + " " +  str(hochtarif[0].Währung)
+        else:
+            # Niedertarif / Nacht
+            f = "PartitionKey eq 'Niedertarif' and Year eq " + str(oi.year)
+            try:
+                niedertarif = table_service.query_entities(t_preise , filter=f , timeout=60)
+                niedertarif = list(niedertarif)   
+            except:
+                logging.warning("failed to load adress data from azure table " + t_preise)
+
+            tarif = "Niedertarif / Nacht"
+            preis = str(niedertarif[0].Preis) + " " +  str(niedertarif[0].Währung)
+            total += (niedertarif[0].Preis * oi.value)
+            subtotal = str(round(niedertarif[0].Preis * oi.value,2)) + " " +  str(niedertarif[0].Währung)
+
+        value = str(oi.value) + " " + str(oi.unit)
+        inv.newEntity(line,MONTHS[oi.month] ,tarif, value ,preis ,subtotal)
+        line += 1
 
 
-
-# Generate Invoice for 272-1
-# Get Invoice information
-f = "PartitionKey eq '272-4' and cleared eq false"        
-try:
-    open_invoice = table_service.query_entities(TABLE_STROMBEZUG, filter=f , timeout=60)
-except:
-    logging.warning("failed to load adress data from azure table Bankverbindung")
-
-total = 0
-line = 1
-open_invoice = list(open_invoice)
-inv = generate_invoice(address_kunde=adressData272_1,address_an=adressData272_4,bank=bankData)
-
-for oi in open_invoice:
-    # Preise ------------------------------------------------
     # Grundgebühr
-    f = "PartitionKey eq 'Grundgebühr' and Year eq " + str(oi.year)
-    try:
-        grundgebühr = table_service.query_entities(TABLE_PREISE, filter=f , timeout=60)
-        grundgebühr = list(grundgebühr) 
-    except:
-        logging.warning("failed to load adress data from azure table " + TABLE_PREISE)
-    # Hochtarif / Tag
-    f = "PartitionKey eq 'Hochtarif' and Year eq " + str(oi.year)
-    try:
-        hochtarif = table_service.query_entities(TABLE_PREISE, filter=f , timeout=60)
-        hochtarif = list(hochtarif)
-    except:
-        logging.warning("failed to load adress data from azure table " + TABLE_PREISE)
-    # Niedertarif / Nacht
-    f = "PartitionKey eq 'Niedertarif' and Year eq " + str(oi.year)
-    try:
-        niedertarif = table_service.query_entities(TABLE_PREISE , filter=f , timeout=60)
-        niedertarif = list(niedertarif)   
-    except:
-        logging.warning("failed to load adress data from azure table " + TABLE_PREISE)
+    periode = MONTHS[open_invoice[0].month] + " - " + MONTHS[open_invoice[-1].month]
+    anzMonate = round((line -1) /2)
+    preis = str(grundgebühr[0].Preis) + " " +  str(grundgebühr[0].Währung)
+    total += (grundgebühr[0].Preis / 12 * anzMonate)
+    subtotal = str(round(grundgebühr[0].Preis / 12 * anzMonate,2)) + " " +  str(grundgebühr[0].Währung)
 
-    if "day" in oi.tarif:
-        tarif = "Hochtarif / Tag"
-        preis = str(hochtarif[0].Preis) + " " +  str(hochtarif[0].Währung)
-        total += (hochtarif[0].Preis * oi.value)
-        subtotal = str(round(hochtarif[0].Preis * oi.value,2)) + " " +  str(hochtarif[0].Währung)
-    else:
-        tarif = "Niedertarif / Nacht"
-        preis = str(niedertarif[0].Preis) + " " +  str(niedertarif[0].Währung)
-        total += (niedertarif[0].Preis * oi.value)
-        subtotal = str(round(niedertarif[0].Preis * oi.value,2)) + " " +  str(niedertarif[0].Währung)
-
-    value = str(oi.value) + " " + str(oi.unit)
-    inv.newEntity(line,MONTHS[oi.month] ,tarif, value ,preis ,subtotal)
+    inv.newEntity(line,periode ,"Grundgebühr pro Jahr", (str(anzMonate) + " Monate") , preis , subtotal)
     line += 1
+    inv.newLine(line)
+    line += 1
+    total = round(total,2)
+    inv.drawTotal(line,total, niedertarif[0].Währung)
+    inv.drawTotalFancy(total, niedertarif[0].Währung)
+    inv.save()
 
+adressData272_1 = getAdressData(TABLE_ADRESSEN,'272-1','1')
+adressData272_2 = getAdressData(TABLE_ADRESSEN,'272-2','2')
+adressData272_3 = getAdressData(TABLE_ADRESSEN,'272-3','3')
+adressData272_4 = getAdressData(TABLE_ADRESSEN,'272-4','4')
 
-# Grundgebühr
-periode = MONTHS[open_invoice[0].month] + " - " + MONTHS[open_invoice[-1].month]
-anzMonate = round((line -1) /2)
-preis = str(grundgebühr[0].Preis) + " " +  str(grundgebühr[0].Währung)
-total += (grundgebühr[0].Preis / 12 * anzMonate)
-subtotal = str(round(grundgebühr[0].Preis / 12 * anzMonate,2)) + " " +  str(grundgebühr[0].Währung)
+bankData = getBankData(TABLE_BANKVERBINDUNG,'Universalkonto','1')
 
-inv.newEntity(line,periode ,"Grundgebühr pro Jahr", (str(anzMonate) + " Monate") , preis , subtotal)
-line += 1
-inv.newLine(line)
-line += 1
-total = round(total,2)
-inv.drawTotal(line,total, niedertarif[0].Währung)
-inv.drawTotalFancy(total, niedertarif[0].Währung)
-inv.save()
-
-
-
-
+#getInvoice(TABLE_STROMBEZUG,TABLE_PREISE,adressData272_1,adressData272_4,bankData)
+#getInvoice(TABLE_STROMBEZUG,TABLE_PREISE,adressData272_2,adressData272_4,bankData)
+#getInvoice(TABLE_STROMBEZUG,TABLE_PREISE,adressData272_3,adressData272_4,bankData)
+getInvoice(TABLE_STROMBEZUG,TABLE_PREISE,adressData272_4,adressData272_4,bankData)
